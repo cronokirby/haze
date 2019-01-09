e3aa07012987e472dbf03321948935a6d65553a7
Lúcás Meier <cronokirby@gmail.com>
2019-01-09 18:27:37 +0100

    WIP: Add writePieces for single files

--- src/Haze/PieceWriter.hs
+++ src/Haze/PieceWriter.hs
@@ -16,12 +16,18 @@ import           Relude
 import           Data.Array                     ( (!)
                                                 , listArray
                                                 )
+import qualified Data.ByteString.Lazy          as LBS
 import           Path                           ( Path
-                                                , Rel
+                                                , Abs
                                                 , File
+                                                , Dir
+                                                , (</>)
                                                 )
-import qualified Path                          
+import qualified Path
 import qualified Path.IO                       as Path
+import           System.IO                      ( Handle
+                                                , IOMode(..)
+                                                )
 
 import           Haze.Tracker                   ( FileInfo(..)
                                                 , FileItem(..)
@@ -29,26 +35,57 @@ import           Haze.Tracker                   ( FileInfo(..)
                                                 )
 
 
--- Maybe make this take a base file path?
-{- | Write all pieces to disk, given a file specification to follow
+{- | Represents information about the pieces we'll be writing
 
-For pieces part of an incomplete file, the data is saved as "piece-1.bin".
-For pieces that overlap between 2 files, the piece is saved in
-"fileA-start.bin" for the piece starting "fileA", and "fileB-end.bin"
-for the piece ending "fileB". 
+This information is usually static, and provided to us before
+we start the piece writer.
 -}
-writePieces :: MonadIO m => FileInfo -> SHAPieces -> [(Int, ByteString)] -> m ()
-writePieces (SingleFile item) (SHAPieces pieceSize _) pieces = do
-    let (FileItem path fileLength _) = item
-        maxPiece = fromIntegral $ (fileLength - 1) `div` pieceSize
-    -- this shouldn't ever throw
-    paths <- forM [0 .. maxPiece] makePiecePath
-    let piecePaths = listArray (0, maxPiece) paths
-    forM_ pieces $ \(piece, bytes) -> do
-        writeFileBS (Path.fromRelFile (piecePaths ! piece)) bytes
-    allPieces <- allM Path.doesFileExist paths
-    return ()
+data PieceInfo = PieceInfo
+    { pieceInfoFile :: FileInfo -- ^ The file info for these pieces
+    , pieceInfoSHA :: SHAPieces -- ^ The piece size and sha for these pieces
+    }
+
+-- | Get the size of an individual piece from the information.
+pieceInfoSize :: PieceInfo -> Int64
+pieceInfoSize (PieceInfo _ (SHAPieces size _)) = size
+
+
+{- | Write a list of complete indices and pieces to a file.
+
+This function takes information about the pieces, telling
+it how they're arranged into files, as well as the size of each normal piece.
+The function takes an absolute directory to serve as the root for all files.
+-}
+writePieces
+    :: MonadIO m => PieceInfo -> Path Abs Dir -> [(Int, ByteString)] -> m ()
+writePieces pieceInfo root pieces = case pieceInfoFile pieceInfo of
+    SingleFile item -> do
+        let (FileItem path fileLength _) = item
+            pieceSize = pieceInfoSize pieceInfo
+            maxPiece = fromIntegral $ (fileLength - 1) `div` pieceSize
+        -- this shouldn't ever throw
+        paths <- forM [0 .. maxPiece] (makePiecePath root)
+        let piecePaths = listArray (0, maxPiece) paths
+        forM_ pieces $ \(piece, bytes) ->
+            writeFileBS (Path.fromAbsFile (piecePaths ! piece)) bytes
+        allPieces <- allM Path.doesFileExist paths
+        when allPieces
+            $ withAbsFile (root </> path) AppendMode (appendAll paths)
+    -- TODO: define this
+    MultiFile _ _ -> undefined
   where
-    makePiecePath :: MonadIO m => Int -> m (Path Rel File)
-    makePiecePath piece =
-        liftIO . Path.parseRelFile $ "piece-" ++ show piece ++ ".bin"
+    makePiecePath :: MonadIO m => Path Abs Dir -> Int -> m (Path Abs File)
+    makePiecePath theRoot piece =
+        let pieceName = "piece-" ++ show piece ++ ".bin"
+        in  (theRoot </>) <$> liftIO (Path.parseRelFile pieceName)
+    appendH :: MonadIO m => Handle -> Path Abs File -> m ()
+    appendH h path =
+        withAbsFile path ReadMode (LBS.hGetContents >=> LBS.hPut h)
+    appendAll :: [Path Abs File] -> Handle -> IO ()
+    appendAll paths = forM_ paths . appendH
+
+
+-- | Utility function for `withFile` but with an absolute path
+withAbsFile :: MonadIO m => Path Abs File -> IOMode -> (Handle -> IO ()) -> m ()
+withAbsFile path mode action =
+    liftIO $ withFile (Path.fromAbsFile path) mode action
