aaaaf7fdca8f9f723fd32a732d47638d268f1899
Lúcás Meier <cronokirby@gmail.com>
2019-01-10 22:40:13 +0100

    Refactors PieceWriter to use more structured data types

--- src/Haze/PieceWriter.hs
+++ src/Haze/PieceWriter.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RecordWildCards #-}
 {- |
 Description: Contains functions centered around writing pieces to disk
 
@@ -7,16 +8,21 @@ in a shared buffer to disk. Utility functions for doing
 the writing, as well as starting up the process are provided.
 -}
 module Haze.PieceWriter
-    ( writePieces -- exported mainly for testing
+    ( -- Mainly exported for testing
+      makePieceInfo
+    , writePieces
     )
 where
 
 import           Relude
 
-import           Data.Array                     ( (!)
+import           Data.Array                     ( Array
+                                                , (!)
+                                                , elems
                                                 , listArray
                                                 )
 import qualified Data.ByteString.Lazy          as LBS
+import           Data.Maybe                     ( fromJust )
 import           Path                           ( Path
                                                 , Abs
                                                 , File
@@ -35,19 +41,47 @@ import           Haze.Tracker                   ( FileInfo(..)
                                                 )
 
 
-{- | Represents information about the pieces we'll be writing
+{- | Represents information about the pieces we'll be writing.
 
-This information is usually static, and provided to us before
-we start the piece writer.
+This should ideally be generated statically before running the piece writer,
+as the information never changes.
 -}
 data PieceInfo = PieceInfo
-    { pieceInfoFile :: FileInfo -- ^ The file info for these pieces
-    , pieceInfoSHA :: SHAPieces -- ^ The piece size and sha for these pieces
+    { pieceInfoRoot :: !(Path Abs Dir) -- ^ The root directory to work in
+    -- | The structure of the pieces we're working with
+    , pieceInfoStructure :: !PieceStructure
     }
 
--- | Get the size of an individual piece from the information.
-pieceInfoSize :: PieceInfo -> Int64
-pieceInfoSize (PieceInfo _ (SHAPieces size _)) = size
+-- | Represents information about the structure of pieces we have.
+data PieceStructure
+    -- | We have a single file, and an array of pieces to save
+    = SimplePieces !(Path Abs File) !(Array Int (Path Abs File))
+
+
+{- | Construct a 'PieceInfo' given information about the pieces.
+
+The 'FileInfo' provides information about how the pieces are organised
+in a file, and the 'SHAPieces' gives us information about how
+each piece is sized. This function also takes a root directory
+into which the files should be unpacked.
+-}
+makePieceInfo :: FileInfo -> SHAPieces -> Path Abs Dir -> PieceInfo
+makePieceInfo fileInfo pieces root = case fileInfo of
+    SingleFile (FileItem path fileLength _) ->
+        let pieceSize  = shaPieceSize pieces
+            maxPiece   = fromIntegral $ (fileLength - 1) `div` pieceSize
+            paths      = makePiecePath root <$> [0 .. maxPiece]
+            piecePaths = listArray (0, maxPiece) paths
+        in  PieceInfo root (SimplePieces (root </> path) piecePaths)
+    -- TODO: define this
+    MultiFile _ _ -> undefined
+  where
+    makePiecePath :: Path Abs Dir -> Int -> Path Abs File
+    makePiecePath theRoot piece =
+        let pieceName = "piece-" ++ show piece ++ ".bin"
+        in  theRoot </> fromJust (Path.parseRelFile pieceName)
+    shaPieceSize :: SHAPieces -> Int64
+    shaPieceSize (SHAPieces pieceSize _) = pieceSize
 
 
 {- | Write a list of complete indices and pieces to a file.
@@ -56,36 +90,24 @@ This function takes information about the pieces, telling
 it how they're arranged into files, as well as the size of each normal piece.
 The function takes an absolute directory to serve as the root for all files.
 -}
-writePieces
-    :: MonadIO m => PieceInfo -> Path Abs Dir -> [(Int, ByteString)] -> m ()
-writePieces pieceInfo root pieces = case pieceInfoFile pieceInfo of
-    SingleFile item -> do
-        let (FileItem path fileLength _) = item
-            pieceSize = pieceInfoSize pieceInfo
-            maxPiece = fromIntegral $ (fileLength - 1) `div` pieceSize
-        -- this shouldn't ever throw
-        paths <- forM [0 .. maxPiece] (makePiecePath root)
-        let piecePaths = listArray (0, maxPiece) paths
+writePieces :: MonadIO m => PieceInfo -> [(Int, ByteString)] -> m ()
+writePieces PieceInfo {..} pieces = case pieceInfoStructure of
+    SimplePieces filePath piecePaths -> do
         forM_ pieces $ \(piece, bytes) ->
             writeFileBS (Path.fromAbsFile (piecePaths ! piece)) bytes
+        let paths = elems piecePaths
         allPieces <- allM Path.doesFileExist paths
-        when allPieces
-            $ withAbsFile (root </> path) AppendMode (appendAll paths)
-    -- TODO: define this
-    MultiFile _ _ -> undefined
-  where
-    makePiecePath :: MonadIO m => Path Abs Dir -> Int -> m (Path Abs File)
-    makePiecePath theRoot piece =
-        let pieceName = "piece-" ++ show piece ++ ".bin"
-        in  (theRoot </>) <$> liftIO (Path.parseRelFile pieceName)
-    appendH :: MonadIO m => Handle -> Path Abs File -> m ()
-    appendH h path =
-        withAbsFile path ReadMode (LBS.hGetContents >=> LBS.hPut h)
-    appendAll :: [Path Abs File] -> Handle -> IO ()
-    appendAll paths = forM_ paths . appendH
+        when allPieces $ withAbsFile filePath AppendMode (appendAll paths)
 
 
 -- | Utility function for `withFile` but with an absolute path
 withAbsFile :: MonadIO m => Path Abs File -> IOMode -> (Handle -> IO ()) -> m ()
 withAbsFile path mode action =
     liftIO $ withFile (Path.fromAbsFile path) mode action
+
+-- | Append all paths in a file to a handle
+appendAll :: [Path Abs File] -> Handle -> IO ()
+appendAll paths = forM_ paths . appendH
+  where
+    moveBytes h = LBS.hGetContents >=> LBS.hPut h
+    appendH h path = withAbsFile path ReadMode (moveBytes h)
