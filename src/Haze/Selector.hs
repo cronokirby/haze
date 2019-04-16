8167f6d941127ca1aba7ab2563096387504eb345
Lúcás Meier <cronokirby@gmail.com>
2019-04-16 14:25:45 +0200

    Add selector logic
    This adds more or less complete logic for selecting the right peers.
    What's missing is handling incoming messages from the peers when
    they become interested, as well as optimistic unchokes.

--- src/Haze/Selector.hs
+++ src/Haze/Selector.hs
@@ -25,6 +25,7 @@ import           Data.RateWindow                ( RateWindow
 import           Haze.Messaging                 ( SelectorToPeer(..) )
 import           Haze.PeerInfo                  ( PeerInfo(..)
                                                 , PeerSpecific(..)
+                                                , PeerFriendship(..)
                                                 )
 import           Haze.Tracker                   ( Peer )
 
@@ -33,14 +34,14 @@ import           Haze.Tracker                   ( Peer )
 data SelectorInfo = SelectorInfo
     { selectorPeerInfo :: !PeerInfo -- | Information on peers
     -- | The set of peers we've chosen to download to
-    , selectorDownloaders :: !(HS.HashSet Peer)
+    , selectorDownloaders :: !(TVar (HS.HashSet Peer))
     {- | The set of peers we'd like to download from us
 
     A peer can join this set either because it was chosen
     at randomly optimistically, or because it had a better
     download rate.
     -}
-    , selectorUninterested :: !(HS.HashSet Peer)
+    , selectorUninterested :: !(TVar (HS.HashSet Peer))
     }
 
 newtype SelectorM a = SelectorM (ReaderT SelectorInfo IO a)
@@ -58,11 +59,30 @@ selectPeers = do
     peerRates <- forM (HM.toList peerMap) $ \(peer, spec) -> do
         rate <- extractRate (peerDLRate spec)
         return (peer, rate)
-    let bestPeers = take 4 . map fst $ sortBy (flip compare `on` snd) peerRates
-    atomically . forM_ bestPeers $ \peer -> do
-        let spec = fromJust $ HM.lookup peer peerMap
-        writeTBQueue (peerFromSelector spec) PeerUnchoke
+    let sortedPeers = sortBy (flip compare `on` snd) peerRates
+        isInterested peer = do
+            let spec = fromJust $ HM.lookup peer peerMap
+            friendship <- readTVarIO $ peerFriendship spec
+            return (peerIsInterested friendship)
+    interestedPeers <- filterM (isInterested . fst) sortedPeers
+    let bestRate = fromMaybe 0.0 . viaNonEmpty head $ map snd interestedPeers
+        newDownloaders = HS.fromList . map fst $ take 4 interestedPeers
+        better =
+            HS.fromList . map fst $ takeWhile ((> bestRate) . snd) sortedPeers
+        newWatched = HS.difference better newDownloaders
+    downloaders  <- asks selectorDownloaders
+    uninterested <- asks selectorUninterested
+    atomically $ do
+        writeTVar downloaders  newDownloaders
+        writeTVar uninterested newWatched
+        forM_ newDownloaders (sendToPeer peerMap PeerUnchoke)
+        forM_ newWatched     (sendToPeer peerMap PeerWatchForInterest)
   where
+    sendToPeer
+        :: HM.HashMap Peer PeerSpecific -> SelectorToPeer -> Peer -> STM ()
+    sendToPeer mp msg peer = do
+        let q = peerFromSelector . fromJust $ HM.lookup peer mp
+        writeTBQueue q msg
     extractRate :: MonadIO m => TVar RateWindow -> m Double
     extractRate var = do
         now <- liftIO getCurrentTime
