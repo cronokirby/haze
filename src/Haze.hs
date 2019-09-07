45102cddae0c6704bb229d9a187c217eafa8ce9b
Lúcás Meier <cronokirby@gmail.com>
2019-09-07 11:54:40 -0400

    Add example code

--- src/Haze.hs
+++ src/Haze.hs
@@ -9,6 +9,8 @@ import           Relude
 import           Haze.Client                    ( launchClient )
 import           Haze.Config                    ( parseConfig )
 
+test :: Int
+test = 3
 
 runHaze :: IO ()
 runHaze = parseConfig >>= launchClient
